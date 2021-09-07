e615e028b1a6423f413a516a3443a4a4f0dc2ad8
ybnr1130 <muuckk@gmail.com>
2021-09-07 11:35:27 +0000

    [Add]Favorites_javascript

--- bookers/app/controllers/favorites_controller.rb
+++ bookers/app/controllers/favorites_controller.rb
@@ -1,17 +1,15 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_to book_path(book)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.find_by(user_id: cuuser.id)
     favorite.destroy
-    redirect_to book_path(book)
   end
 
 end
