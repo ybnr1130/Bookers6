701ef969b8983213f9750dcb477b75dfba66dab5
ybnr1130 <muuckk@gmail.com>
2021-09-08 17:12:55 +0000

    [Fix]一部修正

--- bookers/app/controllers/favorites_controller.rb
+++ bookers/app/controllers/favorites_controller.rb
@@ -8,7 +8,7 @@ class FavoritesController < ApplicationController
 
   def destroy
     @book = Book.find(params[:book_id])
-    favorite = @book.favorites.find_by(user_id: cuuser.id)
+    favorite = @book.favorites.find_by(user_id: current_user.id)
     favorite.destroy
   end
 
